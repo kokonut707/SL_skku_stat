ed6bd74d86555f65d35259f17c1cc23bd2306a4b
kokonut707 <parkminji0725@naver.com>
2021-10-13 16:12:42 +0900

    skku-stat

---
+++ lib.js
@@ -0,0 +1,21 @@
+function sum(numbers) {
+    let s = 0;
+    for (let i = 0; i < numbers.length; i++) s += numbers[i];
+    return s;
+}
+
+function avg(numbers) {
+    return sum(numbers) / numbers.length;
+}
+
+function max(numbers) {
+    let m = numbers[0];
+    for (let i = 1; i < numbers.length; i++) if (m < numbers[i]) m = numbers[i];
+    return m;
+}
+
+module.exports = {
+    sum,
+    avg,
+    max,
+}
---
+++ main.test.js
@@ -0,0 +1,26 @@
+const { test, expect } = require("@jest/globals");
+const { spawn } = require("child_process");
+
+test("Insufficient params", () => {
+    const main = spawn("node", ["main.js", "avg"]);
+    const outputs = [];
+    main.stdout.on("data", (output) => {
+        outputs.push(output);
+    });
+    main.stdout.on("end", () => {
+        const output = outputs.join("").trim();
+        expect(output).toBe("Insufficient parameter!");
+    });
+});
+
+test("wrong command", () => {
+    const main = spawn("node", ["main.js", "count", "0"]);
+    const outputs = [];
+    main.stdout.on("data", (output) => {
+        outputs.push(output);
+    });
+    main.stdout.on("end", () => {
+        const output = outputs.join("").trim();
+        expect(output).toBe("Wrong command!");
+    });
+});
